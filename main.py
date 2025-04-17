73addab44b4bc3e2cc869f65204949d3d1bf7940
Dharansh-Neema <dharanshneema@gmail.com>
2025-04-18 02:24:08 +0530

    GET route added

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from graph_workflow import invoke_graph
 
 app = FastAPI()
 
+@app.get("/")
+async def hello():
+    return {"Stock-Sense-API"}
+
 @app.post("/run-graph")
 async def run_graph(state :AgentState):
     result = invoke_graph(state)
